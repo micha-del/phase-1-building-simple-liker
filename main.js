7c45668987fd3d7c26a163911499c6edb073e3f3
Michael Kiros <michael.kiros00@gmail.com>
2023-01-13 15:01:29 -0500

    commit tree 228b01cde08744aacc8262a0d0b0f8cf5515df77

--- main.js
+++ main.js
@@ -3,10 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector('#modal')
+errorModal.classList.add('hidden')
 
+document.addEventListener("DOMContentLoaded", () => {
 
+  const hearts = document.querySelectorAll("span.like-glyph")
+  console.log(hearts)
 
+  hearts.forEach(hearts => hearts.addEventListener("click", callB))
 
+  function callB(hearts) {
+    console.log(hearts.target)
+    mimicServerCall()
+    .then( () => {
+      if (hearts.target.innerText === EMPTY_HEART) {
+        hearts.target.classList.add('activated-heart')
+        hearts.target.innerText = FULL_HEART
+      }
+      else {
+        hearts.target.classList.remove('activated-heart')
+          hearts.target.innerText = EMPTY_HEART
+      }
+    })
+    }})
+    .catch((error) => {
+      const errorMess = document.getElementById("modal")
+      console.log(errorMess)
+      errorMess.className = "show"
+
+      setTimeout(() => {
+        const errorMess = document.getElementById("modal")
+        console.log(errorMess)
+        errorMess.className = "hidden"}, 3000)})
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
